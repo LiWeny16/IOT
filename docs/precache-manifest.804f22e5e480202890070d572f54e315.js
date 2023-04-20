3bd6a9ad154bd22641a3296a44837c1e07ded9ba
LiWeny16 <a454888395@gmail.com>
2023-04-20 23:12:25 +0800

    update

---
+++ docs/precache-manifest.804f22e5e480202890070d572f54e315.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "26216fc8a690b04bb546",
+    "url": "/static/css/main.3330e392.chunk.css"
+  },
+  {
+    "revision": "26216fc8a690b04bb546",
+    "url": "/static/js/main.26216fc8.chunk.js"
+  },
+  {
+    "revision": "9b9e0d8102c72c9ac5c7",
+    "url": "/static/css/1.d5ecc5a9.chunk.css"
+  },
+  {
+    "revision": "9b9e0d8102c72c9ac5c7",
+    "url": "/static/js/1.9b9e0d81.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b03b5bf788a878b60911d79f1caed93c",
+    "url": "/index.html"
+  }
+];
---
+++ src/Router/fun.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import FloatBtn from "../Components/FloatButton"
+export default class fun extends Component {
+  render() {
+    return (
+      <div>娱乐啦</div>
+    )
+  }
+}
