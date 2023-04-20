4c8462fad50a34f3ab8e6708d04bf9cae686c0d1
LiWeny16 <a454888395@gmail.com>
2023-04-21 03:52:26 +0800

    update

--- src/Router/fun.jsx
+++ src/Router/fun.jsx
@@ -1,9 +1,15 @@
-import React, { Component } from 'react'
-import FloatBtn from "../Components/FloatButton"
+import React, { Component } from "react";
+// import FloatBtn from "../Components/FloatButton"
+import Cube from "../Components/three.js/cube";
 export default class fun extends Component {
   render() {
     return (
-      <div>娱乐啦</div>
-    )
+      <>
+        <div><p className="P2">神奇魔方</p></div>
+        <div className="cube">
+          <Cube />
+        </div>
+      </>
+    );
   }
 }
